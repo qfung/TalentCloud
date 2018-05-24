849e8d0e481f831c44741f8f273509d454e15024
Grant <grant.d.barnes@gmail.com>
2018-05-24 10:56:51 -0400

    Fixed broken ARIA label

--- public_html/inc/applicant/partial-applicant-evidence.php
+++ public_html/inc/applicant/partial-applicant-evidence.php
@@ -87,7 +87,7 @@
         <?php // DEV-NOTE: Note that these triggers have ARIA-Expanded true/false as necessary. ?>
         <div class="applicant-evidence__accordion-trigger active" type="button" tabindex="0" aria-expanded="true">
             <span class="applicant-evidence__accordion-trigger-title">
-                <h4 class="applicant-evidence__accordion-trigger-title-text">Evidence of your HTML skills</h4>
+                <h4 id="applicantEvidenceAccordionText" class="applicant-evidence__accordion-trigger-title-text">Evidence of your HTML skills</h4>
                 <?php // DEV-NOTE: These icons can receive an "active" class to toggle their state in the UI. This should match the icons in the associated tab menu item. ?>
                 <div class="applicant-evidence__accordion-trigger-icon-wrapper">
                     <?php // <i class="fa fa-clipboard active"></i> ?>
