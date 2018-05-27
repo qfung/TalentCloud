01d69c8d4f3db8f8a36a92cbda384d9992d4581f
Grant <grant.d.barnes@gmail.com>
2018-05-27 16:36:50 -0400

    Tab list styles and accessibility

--- public_html/inc/manager/page-profile-other.php
+++ public_html/inc/manager/page-profile-other.php
@@ -1,71 +1,76 @@
 <?php // BEGIN - Edit Admin Profile Form (Page 5 / Other) ?>
 <div id="createEditProfile_step3" class="stepGroup_createEditProfile hidden">
-
-    <div class="tabsWrapper">
-        <div class="tabsSteps">
-            <div class="five-step-tab"><a href="javascript:void(0)" onclick="CreateEditProfileAPI.goToStep('createEditProfile_step1');"><span id="createEditProfileStep1Label_3">About</span></a></div>
-            <div class="five-step-tab"><a href="javascript:void(0)" onclick="CreateEditProfileAPI.goToStep('createEditProfile_step2');"><span id="createEditProfileStep2Label_3">Leadership</span></a></div>
-            <div class="five-step-tab"><a href="javascript:void(0)" onclick="CreateEditProfileAPI.goToStep('createEditProfile_workEnvironment');"><span id="createEditProfileWorkEnvironmentLabel_3">Work Environment</span></a></div>
-            <div class="five-step-tab "><a href="javascript:void(0)" onclick="CreateEditProfileAPI.goToStep('createEditProfile_teamCulture');"><span id="createEditProfileTeamCultureLabel_3">Team Culture</span></a></div>
-            <div class="five-step-tab tab-current"><a href="javascript:void(0)" onclick="CreateEditProfileAPI.goToStep('createEditProfile_step3');"><span id="createEditProfileStep3Label_3">Other</span></a></div>
+    <div class="tabsSteps">
+        <div class="five-step-tab">
+            <button type="button" onclick="CreateEditProfileAPI.goToStep('createEditProfile_step1'); return false;" class="steppedFormLink">
+                <span id="createEditProfileStep1Label_1">About</span>
+            </button>
         </div>
-        <div class="tabs">
-            <div class="steptab inactive"> </div>
-            <div class="steptab inactive"> </div>
-            <div class="steptab inactive"> </div>
-            <div class="steptab inactive"> </div>
-            <div class="steptab active"> </div>
+        <div class="five-step-tab">
+            <button type="button" onclick="CreateEditProfileAPI.goToStep('createEditProfile_step2'); return false;" class="steppedFormLink">
+                <span id="createEditProfileStep2Label_1">Leadership</span>
+            </button>
+        </div>
+        <div class="five-step-tab">
+            <button type="button" onclick="CreateEditProfileAPI.goToStep('createEditProfile_workEnvironment'); return false;" class="steppedFormLink">
+                <span id="createEditProfileWorkEnvironmentLabel_1">Work Environment</span>
+            </button>
+        </div>
+        <div class="five-step-tab">
+            <button type="button" onclick="CreateEditProfileAPI.goToStep('createEditProfile_teamCulture'); return false;" class="steppedFormLink">
+                <span id="createEditProfileTeamCultureLabel_1">Team Culture</span>
+            </button>
+        </div>
+        <div class="five-step-tab tab-current">
+            <button type="button" onclick="CreateEditProfileAPI.goToStep('createEditProfile_step3'); return false;" class="steppedFormLinkActive">
+                <span id="createEditProfileStep3Label_1">Other</span>
+            </button>
         </div>
     </div>
-
-    <br>
+    <!-- Remove?
+    <div class="tabs">
+        <div class="steptab inactive"> </div>
+        <div class="steptab inactive"> </div>
+        <div class="steptab inactive"> </div>
+        <div class="steptab inactive"> </div>
+        <div class="steptab active"> </div>
+    </div>-->
 
     <div class="stepGroupForm">
-
-        <div class="">
-
-            <h3 class="manager-edit-profile__title heading--03">Other Information</h3>
-
-            <div class="manager-edit-profile__required-copy-wrapper">
-                <span>* = </span>
-                <span id="createEditProfile_requiredStep3">Required</span>
-            </div>
-
-            <div class="flex-grid top manager-edit-profile__form">
-
-                <div class="box med-1of2">
-                    <label class="form__label" for="user_manager_profile_work_experience"><span>Work Experience</span></label>
-                    <textarea id="user_manager_profile_work_experience" name="user_manager_profile_work_experience" class="form__textarea textAreaInput"></textarea>
-                </div>
-                <div class="box med-1of2">
-                    <label class="form__label" for="user_manager_profile_work_experience_fr"><span>Work Experience_fr</span></label>
-                    <textarea id="user_manager_profile_work_experience_fr" name="user_manager_profile_work_experience_fr" class="form__textarea textAreaInput"></textarea>
-                </div>
-
-                <div class="box med-1of2">
-                    <label class="form__label" for="user_manager_profile_education"><span>Education</span></label>
-                    <textarea id="user_manager_profile_education" name="user_manager_profile_education" class="form__textarea textAreaInput"></textarea>
-                </div>
-                <div class="box med-1of2">
-                    <label class="form__label" for="user_manager_profile_education_fr"><span>Education_fr</span></label>
-                    <textarea id="user_manager_profile_education_fr" name="user_manager_profile_education_fr" class="form__textarea textAreaInput"></textarea>
-                </div>
-
-            </div>
-
+        <h3 class="manager-edit-profile__title heading--03">Other Information</h3>
+        <div class="manager-edit-profile__required-copy-wrapper">
+            <span>* = </span>
+            <span id="createEditProfile_requiredStep3">Required</span>
         </div>
 
-        <div class="createEditProfileSubmitPane manager-edit-profile__button-form flex-grid">
-            <div class="formGroup box med-1of2">
-                <input id="otherCloseButton" type="button" value="Close" onclick="CreateEditProfileAPI.hideViewProfile();" class="button--grey closeProfileButton">
+        <div class="flex-grid top manager-edit-profile__form">
+            <div class="box med-1of2">
+                <label class="form__label" for="user_manager_profile_work_experience"><span>Work Experience</span></label>
+                <textarea id="user_manager_profile_work_experience" name="user_manager_profile_work_experience" class="form__textarea textAreaInput"></textarea>
             </div>
-            <div class="formGroup box med-1of2">
-                <input id="otherBackButton" type="button" value="Back" onclick="CreateEditProfileAPI.goToStep('createEditProfile_teamCulture');" class="button--grey">
-                <input id="createEditProfileSubmitButton" type="button" value="Submit" onclick="CreateEditProfileAPI.validateOther();" class="button--yellow">
+            <div class="box med-1of2">
+                <label class="form__label" for="user_manager_profile_work_experience_fr"><span>Work Experience_fr</span></label>
+                <textarea id="user_manager_profile_work_experience_fr" name="user_manager_profile_work_experience_fr" class="form__textarea textAreaInput"></textarea>
+            </div>
+            <div class="box med-1of2">
+                <label class="form__label" for="user_manager_profile_education"><span>Education</span></label>
+                <textarea id="user_manager_profile_education" name="user_manager_profile_education" class="form__textarea textAreaInput"></textarea>
+            </div>
+            <div class="box med-1of2">
+                <label class="form__label" for="user_manager_profile_education_fr"><span>Education_fr</span></label>
+                <textarea id="user_manager_profile_education_fr" name="user_manager_profile_education_fr" class="form__textarea textAreaInput"></textarea>
             </div>
         </div>
-
     </div>
 
+    <div class="createEditProfileSubmitPane manager-edit-profile__button-form flex-grid">
+        <div class="formGroup box med-1of2">
+            <input id="otherCloseButton" type="button" value="Close" onclick="CreateEditProfileAPI.hideViewProfile();" class="button--grey closeProfileButton">
+        </div>
+        <div class="formGroup box med-1of2">
+            <input id="otherBackButton" type="button" value="Back" onclick="CreateEditProfileAPI.goToStep('createEditProfile_teamCulture');" class="button--grey">
+            <input id="createEditProfileSubmitButton" type="button" value="Submit" onclick="CreateEditProfileAPI.validateOther();" class="button--yellow">
+        </div>
+    </div>
 </div>
 <?php // END - Edit Admin Profile Form (Page 5 / Other) ?>
--- public_html/inc/manager/page-profile.php
+++ public_html/inc/manager/page-profile.php
@@ -1,76 +1,44 @@
 <!-- BEGIN - Edit Admin Profile Section -->
-<section id="createEditProfileSection" class="pageContent hidden">
+<section id="createEditProfileSection" class="hidden">
+    <div class="block-container">
 
-    <div class="pageBody">
-
-        <div class="block-container">
-
-            <div id="createEditProfilePicWrapper" class="profileBasicInfoTopBar flexContainerVerticallyCentered">
-                <div class="flexLeftOfCenter"></div>
-                <img id="myProfilePic" class="profilePicLarge" src="../images/user.png" alt="Profile Pic"/>
-                <div class="flexRightOfCenter">
-                    <a href="javascript:void(0)" id="editMyProfilePic" onclick="CreateEditProfileAPI.showUploadProfilePic()">
-                        <img id="editMyProfilePicImg" src="../images/edit_profile_pic.svg" alt="Edit Basic Info" class="editImage"/>
-                    </a>
-                </div>
+        <div id="createEditProfilePicWrapper" class="profileBasicInfoTopBar flexContainerVerticallyCentered">
+            <div class="flexLeftOfCenter"></div>
+            <img id="myProfilePic" class="profilePicLarge" src="../images/user.png" alt="Profile Pic"/>
+            <div class="flexRightOfCenter">
+                <button type="button" id="editMyProfilePic" onclick="CreateEditProfileAPI.showUploadProfilePic()" class="profile__edit-trigger">
+                    <span class="hidden">Edit my profile picture</span>
+                    <i class="fa fa-pencil-square"></i>
+                </button>
             </div>
+        </div>
 
-            <div class="wb-frmvld wb-init">
+        <div class="wb-frmvld wb-init section">
+            <!-- Where the old steps resided -->
+            <form method="post" name="CreateEditProfileForm" id="CreateEditProfileForm">
+                <input type="hidden" id="UserId"/>
+                <input type="hidden" id="ManagerProfileId"/>
+                <input type="hidden" id="ManagerProfileDetailsId"/>
 
-                <div class="tabbedForm">
-
-                    <div class="section">
-
-                        <!-- Where the old steps resided -->
-                        <form method="post" name="CreateEditProfileForm" id="CreateEditProfileForm">
-
-                            <input type="hidden" id="UserId"/>
-
-                            <input type="hidden" id="ManagerProfileId"/>
-
-                            <input type="hidden" id="ManagerProfileDetailsId"/>
-
-                            <div id="profileCommon">
-
-                                <div style="text-align: center;width:100%;">
-                                    <div style="width:500px;margin:1em auto;text-align: center;">
-                                        <span id="createEditProfile_name_preview" class="profileName">name</span>
-                                    </div>
-                                </div>
-
-                                <div style="text-align: center;width:100%;">
-                                    <div style="width:500px;margin:1em auto;text-align: center;">
-                                        <span id="createEditProfile_position_preview" class="managerProfileTitle">position</span>
-                                    </div>
-                                </div>
-
-                                <!--<div style="text-align: center;width:100%;">
-                                    <div style="width:500px;margin:1em auto;text-align: center;">
-                                        <span id="createEditProfile_department_preview" class="managerProfileDepartment">department</span>
-                                    </div>
-                                </div>-->
-
-                            </div>
-
-                            <?php
-                                include "page-profile-about-me.php";
-                                include "page-profile-leadership-style.php";
-                                include "page-profile-work-environment.php";
-                                include "page-profile-team-culture.php";
-                                include "page-profile-other.php";
-                            ?>
-                        
-                        </form>
-
-                    </div>
-
+                <div id="profileCommon">
+                    <span id="createEditProfile_name_preview" class="profileName">name</span>
+                    <span id="createEditProfile_position_preview" class="managerProfileTitle">position</span>
+                    <!-- Remove?
+                    <div style="text-align: center;width:100%;">
+                        <div style="width:500px;margin:1em auto;text-align: center;">
+                            <span id="createEditProfile_department_preview" class="managerProfileDepartment">department</span>
+                        </div>
+                    </div>-->
                 </div>
-
-            </div>
-
+                <?php
+                    include "page-profile-about-me.php";
+                    include "page-profile-leadership-style.php";
+                    include "page-profile-work-environment.php";
+                    include "page-profile-team-culture.php";
+                    include "page-profile-other.php"; ?>
+            </form>
         </div>
 
     </div>
-
 </section>
 <!-- END - Edit Admin Profile Section -->
